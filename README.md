49ecdae818c45f6028e1c13506e3733490713721
Denis <connect@alenev.name>
2020-08-14 17:37:36 +0300

    14 08 20 17-38

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# react_app1
+Todo Application
+-----
---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
+
+export default class App extends Component {
+//const App = () => {
+
+  state = {
+	todoData: [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ]
+  };
+ 
+  render() {
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={this.state.todoData} 
+	  onDeleted= { (id) => console.log("delete id ", id) }
+	  />
+    </div>
+  );
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
