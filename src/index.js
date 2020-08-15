6f1750d82a9fb2e9c00d7514566e9bec71223633
Denis <connect@alenev.name>
2020-08-15 12:02:20 +0300

    delete todo list item

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import ItemStatusFilter from './components/item-status-filter';
 import './index.css';
 
 export default class App extends Component {
-//const App = () => {
 
   state = {
 	todoData: [
@@ -18,6 +17,17 @@ export default class App extends Component {
     { label: 'Have a lunch', important: false, id: 3 }
   ]
   };
+  
+  deleteItem = (id) => {
+	  this.setState(({todoData}) => {	   
+       const idx = todoData.findIndex((el) => el.id === id);	   
+	   const newArray = [... todoData.slice(0, idx), ... todoData.slice(idx + 1)];
+	   return {
+	   todoData: newArray
+	   };
+	  }  
+	  );
+  }
  
   render() {
   return (
@@ -29,7 +39,8 @@ export default class App extends Component {
       </div>
 
       <TodoList todos={this.state.todoData} 
-	  onDeleted= { (id) => console.log("delete id ", id) }
+	 // onDeleted= { (id) => console.log("delete id ", id)
+      onDeleted= { this.deleteItem }
 	  />
     </div>
   );
