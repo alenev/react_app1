8a245abfe826e199a86f1bb53688602b08de526a
Denis <connect@alenev.name>
2020-08-17 17:28:57 +0300

    items counters

--- src/index.js
+++ src/index.js
@@ -71,9 +71,14 @@ export default class App extends Component {
   };
  
   render() {
+	  
+	  const doneCount = this.state.todoData.filter(
+	  (el) => el.done).length;
+	  const todoCount = this.state.todoData.length - doneCount;
+	  
   return (
     <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
+      <AppHeader toDo={todoCount} done={doneCount} />
       <div className="top-panel d-flex">
         <SearchPanel />
         <ItemStatusFilter />
